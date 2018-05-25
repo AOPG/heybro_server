cc9201dec90da34b38cc081ff9120ae746315094
wp1573216 <331944508@qq.com>
2018-05-25 08:11:10 +0800

    测试提交

--- admin-ui/src/main/java/com/heybro/CegAdminUIApplication.java
+++ admin-ui/src/main/java/com/heybro/CegAdminUIApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.heybro.mapper")
 public class CegAdminUIApplication {
 
-    public static void main(String[] args) {
-
-        SpringApplication.run(CegAdminUIApplication.class, args);
+    public static void main(String[] args) { SpringApplication.run(CegAdminUIApplication.class, args);
     }
 }
