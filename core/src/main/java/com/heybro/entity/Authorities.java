7caaf98047c13987f2d3c8e9977bf0bb4b445efa
王伟健 <1209050051@qq.com>
2018-06-12 14:40:11 +0800

    生成用户类别

---
+++ core/src/main/java/com/heybro/entity/Authorities.java
@@ -0,0 +1,53 @@
+package com.heybro.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+public class Authorities implements Serializable {
+    private String username;
+
+    private String authority;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    /**
+     * @return authority
+     */
+    public String getAuthority() {
+        return authority;
+    }
+
+    /**
+     * @param authority
+     */
+    public void setAuthority(String authority) {
+        this.authority = authority == null ? null : authority.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", username=").append(username);
+        sb.append(", authority=").append(authority);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
