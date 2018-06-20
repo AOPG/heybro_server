12b5875e2446c1378042dd2f0ba307316cd9818d
wp1573216 <331944508@qq.com>
2018-06-20 10:55:07 +0800

    后台房间信息

--- core/src/main/java/com/heybro/mapper/RoomMapper.java
+++ core/src/main/java/com/heybro/mapper/RoomMapper.java
@@ -1,7 +1,21 @@
 package com.heybro.mapper;
 
 import com.heybro.entity.Room;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.HashMap;
+import java.util.List;
+
 public interface RoomMapper extends Mapper<Room> {
+    /**
+     * 查询房间信息
+     */
+    List<HashMap> RoomInfoList();
+
+
+    /**
+     * 查询房间信息详情
+     */
+    List<HashMap> RoomDetailInfoList(@Param("roomId") Integer roomId);
 }
